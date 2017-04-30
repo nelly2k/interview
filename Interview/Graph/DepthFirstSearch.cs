e9bac4090cc39b4c9d69ed9b91b4a36622adddec
Nelli Lovchikova <nelly2k@live.com>
2017-04-30 18:32:45 +1000

    Depth First Search

---
+++ Interview/Graph/DepthFirstSearch.cs
@@ -0,0 +1,71 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace Interview.Graph
+{
+    public class DepthFirstSearch
+    {
+        private int iterations;
+
+        [SetUp]
+        public void Setup()
+        {
+            iterations = 0;
+        }
+
+        [Test]
+        public void SearchTest()
+        {
+            var s = new GraphNode<char>('S');
+            var b = new GraphNode<char>('B');
+            s.Add(b);
+            s.Add('A','Z','X');
+            b.Add('C');
+
+            Assert.That(Dfs(s,'C'), Is.True);
+            Assert.That(iterations,Is.EqualTo(3));
+        }
+
+        [Test]
+        public void Dfs_With_loop_NotFound()
+        {
+           var a = new GraphNode<char>('A'); 
+            a.Add('D');
+            var t=new GraphNode<char>('T');
+            var b=new GraphNode<char>('B');
+            var c=new GraphNode<char>('C');
+            a.Add(b,c);
+
+            b.Add(t);
+            c.Add(t);
+
+            Assert.That(Dfs(a, 'Z'), Is.False);
+            Assert.That(iterations, Is.EqualTo(5));
+        }
+
+        public bool Dfs<T>(GraphNode<T> root, T data)
+        {
+            iterations++;
+            root.IsVisited = true;
+            if (root.Data.Equals(data))
+            {
+                return true;
+            }
+
+            if (root.Connections == null)
+            {
+                return false;
+            }
+
+            foreach (var node in root.Connections.Where(x=>!x.IsVisited))
+            {
+                if (Dfs<T>(node, data))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Interview/Graph/GraphNode.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Interview.Graph
+{
+    public class GraphNode<T>
+    {
+        public GraphNode(T data)
+        {
+            Data = data;
+        }
+        public List<GraphNode<T>> Connections { get; set; }
+        public bool IsVisited { get; set; }
+        
+        public T Data { get; set; }
+
+        public override string ToString()
+        {
+            return Data.ToString();
+        }
+
+        public void Add(params GraphNode<T>[] nodes)
+        {
+            if (Connections == null)
+            {
+                Connections = new List<GraphNode<T>>();
+            }
+            foreach (var node in nodes)
+            {
+                if (Connections.Any(x => node.Data.Equals(x.Data)))
+                {
+                    throw new Exception("Already exists");
+                }
+
+                Connections.Add(node);
+            }
+           
+        }
+
+        public void Add(params T[] datas)
+        {
+            foreach (var data in datas)
+            {
+                Add(new GraphNode<T>(data));
+            }
+        }
+    }
+}
